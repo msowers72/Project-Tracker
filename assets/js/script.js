a616a965eb76bbbeef784a42169ac0affe64d790
Mike Sowers <msowers72@yahoo.com>
2023-10-12 20:21:25 -0400

    started javascript file using saved references to important DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// save reference to important DOM elemnts
+var timeDisplayEl = $('#time-display');
+var projectDisplayEl = $('#project-display');
+var projectModalEl = $('#project-modal');
+var projectFormEl = $('#project-form');
+var projectNameInputEl = $('#project-name-input');
+var projectTypeInputEl = $('#project-type-input');
+var hourlyRateInputEl = $('#hourly-rate-input');
+var dueDateInputEl = $('#due-date-input');
+
+
+
+
+
+
+
+
+
+
+
