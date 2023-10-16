bfa83a5b452fac1b4185f39a7d0359c54808139f
Mike Sowers <msowers72@yahoo.com>
2023-10-16 14:46:56 -0400

    updating the printProjectData function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,31 @@ var hourlyRateInputEl = $('#hourly-rate-input');
 var dueDateInputEl = $('#due-date-input');
 
 
+
 // handle displaying the time
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a')
     timeDisplayEl.text(rightNow)
 }
 
+// handle printing project data to the page
+function printProjectData(name, type, hourlyRate, dueDate) {
+    var projectRowEl = $('<tr>');
+    
+    var projectNameTdEl = $('<td>').addClass('p-2').text(name);
+    
+    var projectTypeInputEl = $('<td>').addClass('p-2').text(type);
+    
+    var rateTdEl = $('<td>').addClass('p-2').text(hourlyRate);
+    
+    var dueDateTdEl = $('<td>').addClass('p-2').text(dueDate);
 
+    var daysToDate = moment(dueDate, 'MM/DD/YYYY').diff(moment(), 'days');
 
+    var daysLeftTdEl = $('<td>').addClass('p-2').text(daysToDate)
 
+    var totalEarnings = calculateTotalEarnings(hourlyRate, daysToDate)
+}
 
 
 
